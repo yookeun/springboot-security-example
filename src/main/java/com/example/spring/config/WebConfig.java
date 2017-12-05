0eb09395c11c538ed88877abb0f37dd2b92b2e45
Yookeun Kim <knoc720@gmail.com>
2017-12-05 10:21:17 +0900

    설정수정

--- src/main/java/com/example/spring/config/WebConfig.java
+++ src/main/java/com/example/spring/config/WebConfig.java
@@ -33,8 +33,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	    }
 	    
 		@Override
-		public void addViewControllers(ViewControllerRegistry registry) {
-			//registry.addViewController("/").setViewName("login");
+		public void addViewControllers(ViewControllerRegistry registry) {			
 			registry.addViewController("/login").setViewName("login");
 			
 		}
